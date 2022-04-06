cacfcc0c9d4d6f502fe648b6844d56b4f34cab6b
Student <Student@DESKTOP-VD2CFFR>
2022-04-06 14:43:24 +0900

    20220406

---
+++ homepage/src/main/java/egovframework/let/temp/service/impl/TempMapper.java
@@ -0,0 +1,18 @@
+package egovframework.let.temp.service.impl;
+import java.util.Iterator;
+import java.util.List;
+
+import egovframework.let.cop.bbs.service.Board;
+import egovframework.let.cop.bbs.service.BoardVO;
+import egovframework.let.temp.service.TempVO;
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+import org.springframework.stereotype.Repository;
+
+@Mapper("tempMapper")
+public interface TempMapper {
+	
+	//임시데이터 가져오기
+	TempVO selectTemp(TempVO vo) throws Exception;
+}
